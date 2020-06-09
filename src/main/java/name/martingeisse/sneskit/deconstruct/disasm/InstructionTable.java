1d60ec39e8af3f0d24f982e9505a00e46758e67b
Martin Geisse <martingeisse@googlemail.com>
2020-06-09 17:33:54 +0200

    disassembler, WIP

--- src/main/java/name/martingeisse/sneskit/deconstruct/disasm/InstructionTable.java
+++ src/main/java/name/martingeisse/sneskit/deconstruct/disasm/InstructionTable.java
@@ -4,33 +4,7 @@ public final class InstructionTable {
 
     public static final Entry[] TABLE = new Entry[]{
 
-            // 0x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-
-            // 1x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 00-07
             null,
             null,
             null,
@@ -40,33 +14,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // 2x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-
-            // 3x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 08-0f
             null,
             null,
             null,
@@ -76,15 +24,8 @@ public final class InstructionTable {
             null,
             null,
 
-            // 4x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+
+            // 10-17
             null,
             null,
             null,
@@ -94,7 +35,17 @@ public final class InstructionTable {
             null,
             null,
 
-            // 5x
+            // 18-1f
+            new Entry("CLC", 1),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 20-27
             null,
             null,
             null,
@@ -103,7 +54,69 @@ public final class InstructionTable {
             null,
             null,
             null,
+
+            // 28-2f
             null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 30-37
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 38-3f
+            new Entry("SEC", 1),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 40-47
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 48-4f
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 50-57
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 58-5f
+            new Entry("CLI", 1),
             null,
             null,
             null,
@@ -112,15 +125,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // 6x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 60-67
             null,
             null,
             null,
@@ -130,7 +135,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // 7x
+            // 68-6f
             null,
             null,
             null,
@@ -139,6 +144,18 @@ public final class InstructionTable {
             null,
             null,
             null,
+
+            // 70-77
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // 78-7f
             new Entry("SEI", 1),
             null,
             null,
@@ -148,33 +165,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // 8x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-
-            // 9x
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 80-87
             null,
             null,
             null,
@@ -184,33 +175,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // ax
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-
-            // bx
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 88-8f
             null,
             null,
             null,
@@ -220,33 +185,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // cx
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-
-            // dx
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 90-97
             null,
             null,
             null,
@@ -256,15 +195,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // ex
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
+            // 98-9f
             null,
             null,
             null,
@@ -274,7 +205,7 @@ public final class InstructionTable {
             null,
             null,
 
-            // fx
+            // a0-a7
             null,
             null,
             null,
@@ -283,7 +214,109 @@ public final class InstructionTable {
             null,
             null,
             null,
+
+            // a8-af
             null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // b0-b7
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // b8-bf
+            new Entry("CLV", 1),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // c0-c7
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // c8-cf
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // d0-d7
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // d8-df
+            new Entry("CLD", 1),
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // e0-e7
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // e8-ef
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // f0-f7
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+
+            // f8-ff
+            new Entry("SED", 1),
             null,
             null,
             null,
